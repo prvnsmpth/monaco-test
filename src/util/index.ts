3f40e072b362c78ca576cd955008204d0f105919
Praveen Sampath <praveensampath2508@gmail.com>
2021-07-17 13:16:22 +0530

    Add files for testing diff viewer

---
+++ src/util/index.ts
@@ -0,0 +1,15 @@
+
+export function formattedMultilineString(str: string) {
+  return str
+    .split('\n')
+    .filter((l) => l.length > 0)
+    .map((line, idx) => (line + idx))
+}
+
+export function formattedDiff(diff: string): string {
+  return `Formatted: ${unnecesaryFunc(diff)}`
+}
+
+function unnecesaryFunc(diff: string): number {
+  return diff.length
+}
